9e672b8fd37ba8b230bc1999fb744bf908e9cffa
Rohit Tokala <rtokala@purdue.edu>
2022-05-26 00:12:38 +0400

    Implement Button Behaviour

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let currentColor = '#252422';
+let currSize = 16;
+let currMode = 'color';
+
 function createBoxes(lenSide) {
     gridContainer.style.gridTemplateColumns = `repeat(${lenSide}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${lenSide}, 1fr)`;
@@ -13,15 +17,75 @@ function createBoxes(lenSide) {
 
 function colorBox(e) {
     if (e.type == 'mouseover' && !mouseDown) return;
-    e.target.style.backgroundColor = '#252422';
+    e.target.style.backgroundColor = currentColor;
+}
+
+function changeSize(size) {
+    currSize = size;
+    reloadGrid();
+}
+
+function reloadGrid() {
+    clearGrid();
+    createBoxes(currSize);
+}
+
+function clearGrid() {
+    gridContainer.innerHTML = '';
+}
+
+function activateMode(nMode) {
+    let currId = '#' + currMode + 'Button';
+    let node = document.querySelector(currId);
+    node.classList.remove('active');
+
+    let newId = '#' + nMode + 'Button';
+    node = document.querySelector(newId);
+    node.classList.add('active');
+    currMode = nMode;
 }
 
 
 const gridContainer = document.querySelector('#container');
+const slider = document.querySelector('.slider');
+const sizeOut = document.querySelector('#boxSize');
+const clearBtn = document.querySelector('#clearButton');
+const colorBtn = document.querySelector('#colorButton');
+const eraseBtn = document.querySelector('#eraseButton');
+const rainBtn = document.querySelector('#rainButton');
+const darkBtn = document.querySelector('#darkButton');
+const lightBtn = document.querySelector('#lightButton');
 
+/*Slider output value*/
+sizeOut.textContent = slider.value + " x " + slider.value;
+slider.onmousemove = function() {
+    sizeOut.textContent = slider.value + " x " + slider.value;
+}
+slider.onchange = (e) => changeSize(e.target.value);
+
+/*Clear Grid Button*/
+clearBtn.onclick = () => reloadGrid();
+
+/*Color Button*/
+colorBtn.onclick = () => activateMode('color');
+
+/*Erase Button*/
+eraseBtn.onclick = () => activateMode('erase');
+
+/*Rainbow Button*/
+rainBtn.onclick = () => activateMode('rain');
+
+/*darker button*/
+darkBtn.onclick = () => activateMode('dark');
+
+/*lighter button*/
+lightBtn.onclick = () => activateMode('light');
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
-createBoxes(20);
+window.onload = () => {
+    createBoxes(currSize);
+    activateMode(currMode);
+}
